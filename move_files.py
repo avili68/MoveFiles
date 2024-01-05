b23c94af61c4547944899849e8f02957c8b6b7bd
Avi Liani <avi.liani@gmail.com>
2024-01-05 21:00:15 +0200

    reformat all string formatting for python2 compatible updating support for python2
    Signed-off-by: Avi Liani <avi.liani@gmail.com>

--- move_files.py
+++ move_files.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
-"""
-This script purpose is to move files from one directory to another
+from __future__ import print_function
 
-this done by copy files, one by one, verify that the file was copied
-and then delete it from the source.
-"""
 # Builtin modules
 import argparse
 import configparser
@@ -14,6 +10,7 @@ import os
 import platform
 import shutil
 import sys
+import time
 
 # 3ed party modules
 
@@ -21,12 +18,14 @@ import sys
 
 # Some global constants
 BASE_NAME = "move_files"
-LOG_FILE = f"{BASE_NAME}.log"
-CFG_FILE = f"{BASE_NAME}.cfg"
+LOG_FILE = "{}.log".format(BASE_NAME)
+CFG_FILE = "{}.cfg".format(BASE_NAME)
 OS = platform.system()
 
+SHARE_TYPES = ["source", "target"]
+
 # Some global variables
-total_files: int = 0
+total_files = 0
 
 # Setting up the log file
 log = logging.getLogger()
@@ -37,25 +36,41 @@ logging.basicConfig(
     filename=LOG_FILE,
 )
 
-# Setting up and reading the configuration file
-# Note: all default variables should be in this file
-# and also optionally some operation specific variables
-config = configparser.ConfigParser()
-config.read(CFG_FILE)
-
 # Setting the commandline arguments of the script
 parser = argparse.ArgumentParser(description="Move files from one directory to another")
 parser.add_argument("-v", "--verbose", action="store_true", help="Run in verbose mode")
 parser.add_argument("-d", "--debug", action="store_true", help="Run in debug mode")
 parser.add_argument(
-    "-c",
-    "--section",
-    action="store",
-    default="DEFAULT",
-    help="Which section to use",
+    "-c", "--section", action="store", default="DEFAULT", help="Which section to use"
+)
+parser.add_argument(
+    "--config", action="store", default=CFG_FILE, help="Use this configuration file"
 )
 
 args = parser.parse_args()
+
+# Setting up and reading the configuration file
+# Note: all default variables should be in this file
+# and also optionally some operation specific variables
+config = configparser.ConfigParser()
+print(args.config)
+
+print(f"Try to read configuration file : {args.config}")
+dataset = config.read(args.config)
+if not dataset:
+    print("Cannot read configuration file")
+
+print(dataset)
+print(config["DEFAULT"])
+for key in config["DEFAULT"]:
+    print(f"{key} : {config['DEFAULT'][key]}")
+
+
+def combine_args():
+    """
+    This function combine the configuration file arguments with cli args
+    """
+    pass
 
 
 def md5(file_name):
@@ -69,7 +84,7 @@ def md5(file_name):
         hex : the file md5sum results
     """
     if not os.path.exists(file_name):
-        print(f"The file '{file_name}' does not exists !")
+        print("The file '{}' does not exists !".format(file_name))
         return None
 
     hash_md5 = hashlib.md5()
@@ -79,74 +94,78 @@ def md5(file_name):
     return hash_md5.hexdigest()
 
 
-def make_connection(op="connect"):
+def make_connection(op="connect", share_type: str = "source"):
     """
     Mounting shared filesystem on Drive/MountPoint
 
     Args:
         op (str): which operation to use connect / disconnect
-
+        share_type (str): the type of the share to connect/disconnect - source / target
     Return:
         bool : does the command succeed or not
     """
 
-    if not config[args.section]["connect"]:
+    if not config[args.section]["connect_{}".format(share_type)]:
         print("No Connection / Disconnecting is needed !")
         return True
 
     extra = "/d" if op == "disconnect" else ""
+
+    # connection
     if extra != "":
         share = ""
+    # disconnection
     else:
-        share = config[args.section]["share"]
+        share = config[args.section]["{}_share".format(share_type)]
 
     if OS == "Windows":
         net_cmd = "C:/Windows/System32/net.exe"
-        cmd = f"{net_cmd} use {extra} {config[args.section]['target_drive']} {share}"
+        cmd = "{} use {} {} {}".format(
+            net_cmd, extra, config[args.section]["{}_drive".format(share_type)], share
+        )
     else:
-        cmd = f"mount -t nfs {config[args.section]['target_drive']} {share}"
+        cmd = "mount -t nfs {} {}".format(
+            config[args.section]["{}_drive".format(share_type)], share
+        )
         if op == "disconnect":
-            cmd = f"umount {config[args.section]['target_drive']}"
+            cmd = "umount {}".format(
+                config[args.section]["{}_drive".format(share_type)]
+            )
 
     return os.system(cmd) == 0
 
 
 def quit_prog():
-    """
-    function to run ion the end of the scripy.
-    it disconnects from the shares(s) / mount(s) and exit
-    """
     log.info("Operation finished")
-    make_connection(op="disconnect")
-    msg_string = (
-        "Finished moving of {config[args.section]['batch_size']} "
-        "files from {config[args.section]['source_path']} "
-        "to {config[args.section]['target_path']} at {time.ctime()}"
-    )
+    for share_type in SHARE_TYPES:
+        make_connection(op="disconnect", share_type=share_type)
 
-    log.info(msg_string)
-    print(msg_string)
-    sys.exit()
+    log.info(
+        "Finished moving of {} ".format(config[args.section]["batch_size"]),
+        "from {} to {} at {}".format(
+            config[args.section]["source_path"],
+            config[args.section]["target_path"],
+            time.ctime(),
+        ),
+    )
+    print(
+        "Finished moving of {} ".format(config[args.section]["batch_size"]),
+        "from {} to {} at {}".format(
+            config[args.section]["source_path"],
+            config[args.section]["target_path"],
+            time.ctime(),
+        ),
+    )
+    exit()
 
 
 def move_files(source_dir, target_dir):
-    """
-    the actual function which move the files.
-    this is a recursive function, if the source is directory, it gets into
-    the directory and called itself.
-
-    Args:
-        source_dir (str): the directory to move file from
-        target_dir (str): the directory to move file into
-    """
     global total_files
 
     all_files = os.listdir(source_dir)
 
     for f in all_files:
-        # For debugging purpose
         total_files += 1
-        # print (f"{total_files}")
         print(".", end="", flush=True)
         if int(config[args.section]["batch_size"]) > 0:
             if total_files > int(config[args.section]["batch_size"]):
@@ -156,25 +175,25 @@ def move_files(source_dir, target_dir):
         trg = os.path.join(target_dir, f)
         ok = True
         if os.path.isdir(src):
-            print("", flush=True)
+            print(total_files, flush=True)
             try:
                 os.mkdir(trg)
             except FileExistsError:
                 pass
-            log.info("%s is a Directory, The Target is %s", src, trg)
+            log.info("{} is a Directory, The Target is {}".format(src, trg))
             move_files(src, trg)
             if len(os.listdir(src)) == 0:
-                log.info("Source dir : %s is empty. Delete it...", src)
+                log.info("Source dir : {} is empty. Delete it...".format(src))
                 try:
                     os.rmdir(src)
                 except Exception as ex:
-                    print(f"can not delete directory - {ex}")
-                    log.info("Delete of Directory %s Failed - %s", src, ex)
+                    print("can not delete directory - {}".format(ex))
+                    log.info("Delete of Directory {} Failed - {}".format(src, ex))
             else:
-                log.info("Not all files in the directory %s was deleted !", src)
+                log.info("Not all files in the directory {} was deleted !".format(src))
 
         else:
-            log.info("Copying the file %s to %s", src, trg)
+            log.info("Copying the file {} to {}".format(src, trg))
             try:
                 shutil.copy2(src, trg)
                 src_md5 = md5(src)
@@ -183,46 +202,53 @@ def move_files(source_dir, target_dir):
                     ok = False
                 if ok:
                     try:
-                        log.info("Deleting %s", src)
+                        log.info("Deleting {}".format(src))
                         os.remove(src)
                     except Exception as ose:
-                        log.error("Deleting of source file %s Failed - %s!", src, ose)
-            except Exception as e:
-                log.error("Copy of %s failed - %s", src, e)
+                        log.error(
+                            "Deleting of source file {} Failed - {}!".format(src, ose)
+                        )
+            except Exception as ex:
+                log.error("Copy of {} failed - {}".format(src, ex))
 
 
 if __name__ == "__main__":
 
     # Verify that all variables are defined.
-    config_ok: bool = True
+    config_ok = True
     args.section = args.section.upper()
 
     try:
         int(config[args.section]["batch_size"])
     except Exception as e:
-        print(f"Error : 'batch_size' must be Integer ! [{e}]")
+        print("Error : 'batch_size' must be Integer ! [{}]".format(e))
         config_ok = False
 
     # Check the source & target paths
-    for sub_key in ["source", "target"]:
-        if not config.has_option(args.section, f"{sub_key}_path"):
-            print(f"Error : The value of '{sub_key}_path' is missing !")
+    for sub_key in SHARE_TYPES:
+        if not config.has_option(args.section, "{}_path".format(sub_key)):
+            print("Error : The value of '{}_path' is missing !".format(sub_key))
             config_ok = False
-        elif config[args.section][f"{sub_key}_path"] == "":
-            print(f"Error : The value of '{sub_key}_path' cannot be empty !")
+        elif config[args.section]["{}_path".format(sub_key)] == "":
+            print("Error : The value of '{}_path' cannot be empty !".format(sub_key))
             config_ok = False
 
     # Check if mount is needed
-    if config.has_option(args.section, "connect"):
-        if config[args.section]["connect"].lower() == "true":
-            for key in ["target_drive", "share"]:
-                if config[args.section][key]:
-                    if not config.has_option(args.section, key):
-                        print(f"Error : The value of '{key}' is missing !")
-                        config_ok = False
-                    elif config[args.section][key]:
-                        print(f"Error : The value of '{key}' cannot be empty !")
-                        config_ok = False
+    for sub_key in SHARE_TYPES:
+        if config.has_option(args.section, "connect_{}".format(sub_key)):
+            if config[args.section]["connect_{}".format(sub_key)].lower() == "true":
+                for key in ["target_drive", "share"]:
+                    if config[args.section][key]:
+                        if not config.has_option(args.section, key):
+                            print("Error : The value of '{}' is missing !".format(key))
+                            config_ok = False
+                        elif config[args.section][key]:
+                            print(
+                                "Error : The value of '{}' cannot be empty !".format(
+                                    key
+                                )
+                            )
+                            config_ok = False
 
     if not config_ok:
         sys.exit(1)
@@ -233,11 +259,13 @@ if __name__ == "__main__":
 
     if args.debug:
         print(
-            f"The List of sections in the configuration file is : {config.sections()}"
+            "The List of sections in the configuration file is : {}".format(
+                config.sections()
+            )
         )
-        print(f"Going to use {args.section} as the variables values :")
+        print("Going to use {} as the variables values :".format(args.section))
         for key in config[args.section]:
-            print(f"  {key} = {config[args.section][key]}")
+            print("  {} = {}".format(key, config[args.section][key]))
 
     # Connecting the target path to the system - if needed
     if not make_connection():
@@ -247,9 +275,10 @@ if __name__ == "__main__":
     move_files(config[args.section]["source_path"], config[args.section]["target_path"])
 
     # Dis-connecting the target path from the system - if needed
-    if not make_connection(op="disconnect"):
-        print("Cannot disconnect the target share from the system")
-        print("You need to disconnect it manually !!!")
+    for sub_key in SHARE_TYPES:
+        if not make_connection(op="disconnect", share_type=sub_key):
+            print("Cannot disconnect the {} share from the system".format(sub_key))
+            print("You need to disconnect it manually !!!")
 
     if args.verbose:
         print("Ending the script")
