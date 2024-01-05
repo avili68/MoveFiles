5889229489a2d907b9b18da76403ee84b2c284f6
Avi Liani <avi.liani@gmail.com>
2024-01-05 21:00:15 +0200

    reformat all string formatting for python2 compatible

--- move_files.py
+++ move_files.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 # Builtin modules
 import argparse
@@ -17,8 +18,8 @@ import time
 
 # Some global constants
 BASE_NAME = "move_files"
-LOG_FILE = f"{BASE_NAME}.log"
-CFG_FILE = f"{BASE_NAME}.cfg"
+LOG_FILE = "{}.log".format(BASE_NAME)
+CFG_FILE = "{}.cfg".format(BASE_NAME)
 OS = platform.system()
 
 # Some global variables
@@ -61,7 +62,7 @@ def md5(file_name):
         hex : the file md5sum results
     """
     if not os.path.exists(file_name):
-        print(f"The file '{file_name}' does not exists !")
+        print("The file '{}' does not exists !".format(file_name))
         return None
 
     hash_md5 = hashlib.md5()
@@ -94,11 +95,11 @@ def make_connection(op="connect"):
 
     if OS == "Windows":
         net_cmd = "C:/Windows/System32/net.exe"
-        cmd = f"{net_cmd} use {extra} {config[args.section]['target_drive']} {share}"
+        cmd = "{} use {} {} {}".format(net_cmd, extra, config[args.section]['target_drive'], share)
     else:
-        cmd = f"mount -t nfs {config[args.section]['target_drive']} {share}"
+        cmd = "mount -t nfs {} {}".format(config[args.section]['target_drive'], share)
         if op == "disconnect":
-            cmd = f"umount {config[args.section]['target_drive']}"
+            cmd = "umount {}".format(config[args.section]['target_drive'])
 
     return os.system(cmd) == 0
 
@@ -107,12 +108,12 @@ def quit_prog():
     log.info("Operation finished")
     make_connection(op="disconnect")
     log.info(
-        f"Finished moving of {config[args.section]['batch_size']} "
-        f"from {config[args.section]['source_path']} to {config[args.section]['target_path']} at {time.ctime()}"
+        "Finished moving of {} ".format(config[args.section]['batch_size']),
+        "from {} to {} at {}".format(config[args.section]['source_path'], config[args.section]['target_path'], time.ctime())
     )
     print(
-        f"Finished moving of {config[args.section]['batch_size']} "
-        f"from {config[args.section]['source_path']} to {config[args.section]['target_path']} at {time.ctime()}"
+        "Finished moving of {} ".format(config[args.section]['batch_size']),
+        "from {} to {} at {}".format(config[args.section]['source_path'], config[args.section]['target_path'], time.ctime())
     )
     exit()
 
@@ -140,20 +141,20 @@ def move_files(source_dir, target_dir):
                 os.mkdir(trg)
             except FileExistsError:
                 pass
-            log.info(f"{src} is a Directory, The Target is {trg}")
+            log.info("{} is a Directory, The Target is {}".format(src, trg))
             move_files(src, trg)
             if len(os.listdir(src)) == 0:
-                log.info(f"Source dir : {src} is empty. Delete it...")
+                log.info("Source dir : {} is empty. Delete it...".format(src))
                 try:
                     os.rmdir(src)
                 except Exception as ex:
-                    print(f"can not delete directory - {ex}")
-                    log.info(f"Delete of Directory {src} Failed - {ex}")
+                    print("can not delete directory - {}".format(ex))
+                    log.info("Delete of Directory {} Failed - {}".format(src, ex))
             else:
-                log.info(f"Not all files in the directory {src} was deleted !")
+                log.info("Not all files in the directory {} was deleted !".format(src))
 
         else:
-            log.info(f"Copying the file {src} to {trg}")
+            log.info("Copying the file {} to {}".format(src, trg))
             try:
                 shutil.copy2(src, trg)
                 src_md5 = md5(src)
@@ -162,12 +163,12 @@ def move_files(source_dir, target_dir):
                     OK = False
                 if OK:
                     try:
-                        log.info(f"Deleting {src}")
+                        log.info("Deleting {}".format(src))
                         os.remove(src)
                     except Exception as ose:
-                        log.error(f"Deleting of source file {src} Failed - {ose}!")
+                        log.error("Deleting of source file {} Failed - {}!".format(src, ose))
             except Exception as e:
-                log.error(f"Copy of {src} failed - {e}")
+                log.error("Copy of {} failed - {}".format(src, e))
 
 
 if __name__ == "__main__":
@@ -179,16 +180,16 @@ if __name__ == "__main__":
     try:
         int(config[args.section]["batch_size"])
     except Exception as e:
-        print(f"Error : 'batch_size' must be Integer ! [{e}]")
+        print("Error : 'batch_size' must be Integer ! [{}]".format(e))
         config_ok = False
 
     # Check the source & target paths
     for sub_key in ["source", "target"]:
-        if not config.has_option(args.section, f"{sub_key}_path"):
-            print(f"Error : The value of '{sub_key}_path' is missing !")
+        if not config.has_option(args.section, "{}_path".format(sub_key)):
+            print("Error : The value of '{}_path' is missing !".format(sub_key))
             config_ok = False
-        elif config[args.section][f"{sub_key}_path"] == "":
-            print(f"Error : The value of '{sub_key}_path' cannot be empty !")
+        elif config[args.section]["{}_path".format(sub_key)] == "":
+            print("Error : The value of '{}_path' cannot be empty !".format(sub_key))
             config_ok = False
 
     # Check if mount is needed
@@ -197,10 +198,10 @@ if __name__ == "__main__":
             for key in ["target_drive", "share"]:
                 if config[args.section][key]:
                     if not config.has_option(args.section, key):
-                        print(f"Error : The value of '{key}' is missing !")
+                        print("Error : The value of '{}' is missing !".format(key))
                         config_ok = False
                     elif config[args.section][key]:
-                        print(f"Error : The value of '{key}' cannot be empty !")
+                        print("Error : The value of '{}' cannot be empty !".format(key))
                         config_ok = False
 
     if not config_ok:
@@ -212,11 +213,11 @@ if __name__ == "__main__":
 
     if args.debug:
         print(
-            f"The List of sections in the configuration file is : {config.sections()}"
+            "The List of sections in the configuration file is : {}".format(config.sections())
         )
-        print(f"Going to use {args.section} as the variables values :")
+        print("Going to use {} as the variables values :".format(args.section))
         for key in config[args.section]:
-            print(f"  {key} = {config[args.section][key]}")
+            print("  {} = {}".format(key, config[args.section][key]))
 
     # Connecting the target path to the system - if needed
     if not make_connection():
